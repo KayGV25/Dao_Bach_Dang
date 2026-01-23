3e8420950bd72346d4aad12e7ea5e3b96856aab6
KayGV25 <khuongvudang25@gmail.com>
2026-01-23 10:46:14 +0700

    (FEAT)[DBD-4] Adding constants

---
+++ constants/nav.ts
@@ -0,0 +1,24 @@
+export const DROPDOWN_CONTENT = {
+  "Trang Chủ": "/",
+  "Về Chúng Tôi": "/about",
+  "Các Ngành": {
+    "Ngành Ấu": {
+      "Sói Bạch Đằng": "/cub-bd",
+      "Sói Vân Cừ": "/cub-vc",
+      "Sói Thủy Nguyên": "/cub-tn",
+      "Sói Nữ Bạch Đằng": "/cub-nbd"
+    },
+    "Ngành Thiếu": {
+      "Thiếu Bạch Đằng": "/sct-bd",
+      "Thiếu Vân Cừ": "/sct-vc",
+      "Thiếu Nữ Bạch Đằng": "sct-nbd"
+    },
+    "Ngành Kha": {
+      "Kha Bạch Đằng": "/vts-bd",
+      "Kha Vân Cừ": "/vts-vc",
+      "Kha Nữ Bạch Đằng": "/vts-nbd",
+      "Kha Thủy Nguyên": "/vts-tn"
+    },
+    "Ngành Tráng": "/rvs"
+  }
+};
