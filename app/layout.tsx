5906a7e4b5c78b7755d164e87e7e91482aedb9c8
KayGV25 <khuongvudang25@gmail.com>
2026-01-27 12:53:10 +0700

    (FEAT)[DBD-26] Add Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NavBar } from '@/components/navbar';
+import { FaLocationArrow } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { IoTime } from "react-icons/io5";
+
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,9 +43,20 @@ export default function RootLayout({
             {children}
           </div>
         </main>
-        <footer className="w-full bg-primary px-8 py-4 flex text-white">
-          <div>
-            asdfasdf
+        <footer className="w-full bg-primary px-8 py-4 flex justify-start text-white text-xs">
+          <div className="flex flex-col gap-4">
+            <Link href="https://maps.app.goo.gl/kaDR5Xo2ZVKxZVeR6" target="_blank" className="flex gap-2">
+              <FaLocationArrow   className="self-center"/>
+              <span>Khu B Công viên Hoàng Văn Thụ, Đ. Hoàng Văn Thụ Đ. Trần Quốc Hoàn, Phường 2, Tân Bình, Thành phố Hồ Chí Minh, Việt Nam</span>
+            </Link>
+            <Link href="https://www.facebook.com/daobachdangofficial" target="_blank" className="flex gap-2">
+              <FaFacebook className="self-center"/>
+              <span>Page Đạo Bạch Đằng</span>
+            </Link>
+            <div className="flex gap-2">
+              <IoTime className="self-center"/>
+              <span>Thời gian hoạt động: 8:30 - 11h30, Chủ Nhật hằng tuần</span>
+            </div>
           </div>
         </footer>
       </body>
