8daedde1d4628b8bd0e5947c2536f44656c05de3
KayGV25 <khuongvudang25@gmail.com>
2026-02-03 17:04:01 +0700

    (FEAT)[DBD-34] Add OG for all pages

---
+++ app/[name]/[detail]/opengraph-image.tsx
@@ -0,0 +1,68 @@
+import { ImageResponse } from "next/og"
+import { CONTENT_LOADERS, ContentKey} from "@/constants/registry"
+import { COMING_SOON } from "@/constants/util";
+import { ComingSoon } from "@/app/_util_section/commingSoon";
+
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = "image/png"
+
+export default async function Image({ params }: { params: Promise<{ name: string, detail: string }> }) {
+  const { detail } = await params
+
+  if (COMING_SOON.includes(detail)) {
+      return new ImageResponse((
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "80px",
+            background: "white",
+            fontFamily: "sans-serif",
+          }}
+        >
+          <h1 style={{ fontSize: 64, fontWeight: "bold"}}>
+            Coming Soon
+          </h1>
+        </div>
+      ), size);
+    }
+
+  const content = await CONTENT_LOADERS[detail as ContentKey]()
+  const paragraphs = content.general.split("\n")
+  
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          padding: "80px",
+          background: "white",
+          fontFamily: "sans-serif",
+        }}
+      >
+        <h1 style={{ fontSize: 64, fontWeight: "bold" }}>
+          {content.title}
+        </h1>
+
+        {paragraphs.map((paragraph, index) => (
+            <p key={index}>
+              {paragraph}
+            </p>
+        ))}
+      </div>
+    ),
+    size
+  )
+}
---
+++ app/[name]/opengraph-image.tsx
@@ -0,0 +1,67 @@
+import { ImageResponse } from "next/og"
+import { CONTENT_LOADERS, ContentKey} from "@/constants/registry"
+import { COMING_SOON } from "@/constants/util";
+
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = "image/png"
+
+export default async function Image({ params }: { params: Promise<{ name: string, detail: string }> }) {
+  const { name } = await params
+
+  if (COMING_SOON.includes(name)) {
+    return new ImageResponse((
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          padding: "80px",
+          background: "white",
+          fontFamily: "sans-serif",
+        }}
+      >
+        <h1 style={{ fontSize: 64, fontWeight: "bold" }}>
+          Coming Soon
+        </h1>
+      </div>
+    ), size);
+  }
+
+  const content = await CONTENT_LOADERS[name as ContentKey]()
+  const paragraphs = content.general.split("\n")
+  
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "80px",
+          background: "white",
+          fontFamily: "sans-serif",
+        }}
+      >
+        <h1 style={{ fontSize: 64, fontWeight: "bold" }}>
+          {content.title}
+        </h1>
+
+        {paragraphs.map((paragraph, index) => (
+            <p key={index}>
+              {paragraph}
+            </p>
+        ))}
+      </div>
+    ),
+    size
+  )
+}
